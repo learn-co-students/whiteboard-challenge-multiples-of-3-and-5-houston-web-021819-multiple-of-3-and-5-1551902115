aca2cbdec02d20b3dd5cad8bf13778296f686ac8
github username <github email address>
2019-03-06 14:35:31 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    multiples = []
+    counter = 0
+      until counter == self.limit - 1
+        counter += 1
+        if counter%3 == 0 || counter%5 ==0
+          multiples << counter
+        end
+      end
+      multiples
+  end
+
+  def sum_multiples
+    self.collect_multiples.inject(:+)
+  end
+
+end
